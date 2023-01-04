86c5cee6de20d79608b1f974db8681102792e9e4
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-01-04 12:37:58 +0200

    Write a summary for methods

--- Task1.CustomQueue/CustomQueue.cs
+++ Task1.CustomQueue/CustomQueue.cs
@@ -13,28 +13,49 @@ namespace Task1.CustomQueue
     public class CustomQueue
     {
         private int[] array;
-        private int head; // The index from which to dequeue if the queue isn't empty.
-        private int tail; // The index at which to enqueue if the queue isn't full.
-        private int MaxCount => array.Length;// Max number of elements.
-        public int Count { get; private set; } // Number of elements.
-
-        // Creates a queue with room for capacity objects. The default initial
-        // capacity and grow factor are used.
+        /// <summary>
+        /// The index from which to dequeue if the queue isn't empty.
+        /// </summary>
+        private int head;
+        /// <summary>
+        /// The index at which to enqueue if the queue isn't full.
+        /// </summary>
+        private int tail;
+        /// <summary>
+        /// Max number of elements.
+        /// </summary>
+        private int MaxCount => array.Length;
+        /// <summary>
+        /// Number of elements.
+        /// </summary>
+        public int Count { get; private set; }
+        /// <summary>
+        ///  Creates a queue with room for capacity objects. The default initial
+        /// capacity and grow factor are used.
+        /// </summary>
         public CustomQueue()
         {
             array = Array.Empty<int>();
 
         }
-        // Creates a queue with room for capacity objects. The default grow factor
-        // is used.
+        /// <summary>
+        /// Creates a queue with room for capacity objects. The default grow factor
+        /// is used.
+        /// </summary>
+        /// <param name="capacity"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public CustomQueue(int capacity)
         {
             if (capacity < 0)
                 throw new ArgumentOutOfRangeException("Capacity out of range exception");
             array = new int[capacity];
         }
-        // Increments the index wrapping it if necessary.
-        // Moves the enumerator to the next element of the queue
+        /// <summary>
+        /// Increments the index wrapping it if necessary.
+        /// Moves the enumerator to the next element of the queue.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public void MoveNext(ref int index)
         {
             if (index < 0 || index > MaxCount)
@@ -48,8 +69,11 @@ namespace Task1.CustomQueue
             }
             index = tmp;
         }
-        //  Grows or shrinks the buffer to hold capacity objects.Capacity
-        // must be >= count.
+        /// <summary>
+        /// Grows or shrinks the buffer to hold capacity objects.Capacity
+        /// must be >= count.
+        /// </summary>
+        /// <param name="capacity"></param>
         private void SetCapacity(int capacity)
         {
             int[] newarray = new int[capacity];
@@ -85,7 +109,10 @@ namespace Task1.CustomQueue
             if (newcapacity < capacity) newcapacity = capacity;
             SetCapacity(newcapacity);
         }
-        // Adds item to the tail of the queue.
+        /// <summary>
+        /// Adds item to the tail of the queue.
+        /// </summary>
+        /// <param name="item"></param>
         public void Enqueue(int item)
         {
             if (Count == MaxCount)
@@ -96,9 +123,11 @@ namespace Task1.CustomQueue
             MoveNext(ref tail);
             Count++;
         }
-        // Removes the object at the head of the queue and returns it. If the queue
-        // is empty, this method throws an
-        // InvalidOperationException.
+        /// <summary>
+        /// Removes the object at the head of the queue and returns it. 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public int Dequeue()
         {
             int head = this.head;
@@ -112,9 +141,11 @@ namespace Task1.CustomQueue
             Count--;
             return removed;
         }
-        // Returns the object at the head of the queue. The object remains in the
-        // queue. If the queue is empty, this method throws an
-        // InvalidOperationException.
+        /// <summary>
+        /// Returns the object at the head of the queue. The object remains in the queue.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public int Peek()
         {
             if (Count == 0)
@@ -125,8 +156,12 @@ namespace Task1.CustomQueue
             return array[head];
         }
 
-        // Returns true if the queue contains at least one object equal to item.
-        // If the queue is empty, this method throws a false
+        /// <summary>
+        /// Returns true if the queue contains at least one object equal to item.
+        /// If the queue is empty, this method throws a false.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Contains(int item)
         {
             if (Count == 0)
@@ -140,7 +175,11 @@ namespace Task1.CustomQueue
             }
             return false;
         }
-        // CopyTo copies elements from one collection to another
+        /// <summary>
+        /// CopyTo copies elements from one collection to another
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void CopyTo(CustomQueue queue)
         {
             if (queue == null)
@@ -155,28 +194,34 @@ namespace Task1.CustomQueue
                 queue.Enqueue(item);
 
         }
-        // Removes all Objects from the queue.
+        /// <summary>
+        /// Removes all Objects from the queue.
+        /// </summary>
         public void Clear()
         {
             if (Count != 0)
             {
-                    if (head < tail)
-                    {
-                        Array.Clear(array, head, Count);
-                    }
-                    else
-                    {
-                        Array.Clear(array, head, MaxCount - head);
-                        Array.Clear(array, 0, tail);
-                    }
+                if (head < tail)
+                {
+                    Array.Clear(array, head, Count);
+                }
+                else
+                {
+                    Array.Clear(array, head, MaxCount - head);
+                    Array.Clear(array, 0, tail);
+                }
                 Count = 0;
             }
 
             head = 0;
             tail = 0;
         }
-        //Used to check whether two specified objects have the same value or not.
-        //If both objects have same value, it return true otherwise false. 
+        /// <summary>
+        /// Used to check whether two specified objects have the same value or not.
+        ///If both objects have same value, it return true otherwise false.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
         public bool Equals(CustomQueue queue)
         {
             if (queue == null)

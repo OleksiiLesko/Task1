815acb46103b1c0344bd56eb0b1a4911500d30ef
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-01-04 13:46:29 +0200

    Change location CustomQueueTests and CustomStackTests to one folder Task1.Tests

---
+++ Task1.Tests/CustomQueueTests.cs
@@ -0,0 +1,146 @@
+namespace Task1.CustomQueue
+{
+    public class CustomQueueTests
+    {
+        [Fact]
+        public void Clear_RemovesAllItemsOfTheQueue()
+        {
+            var customQueue = new CustomQueue();
+            customQueue.Enqueue(1);
+            customQueue.Enqueue(2);
+            customQueue.Clear();
+            Assert.Equal(0, customQueue.Count);
+        }
+        [Fact]
+        public void Contains_QueueContainsAtLeastOneObjectEqualToItem()
+        {
+            var customQueue = new CustomQueue();
+            customQueue.Enqueue(1);
+            Assert.False(customQueue.Contains(2));
+            Assert.True(customQueue.Contains(1));
+        }
+        [Fact]
+        public void Contains_QueueIsEmpty()
+        {
+            var customStack = new CustomQueue();
+            Assert.False(customStack.Contains(1));
+        }
+        [Fact]
+        public void CopyTo_CopiesElementsFromOneCollectionToAnother()
+        {
+            var customQueue = new CustomQueue();
+            customQueue.Enqueue(1);
+            var newCustomQueue = new CustomQueue();
+            customQueue.CopyTo(newCustomQueue);
+            Assert.True(customQueue.Equals(newCustomQueue));
+        }
+        [Fact]
+        public void CopyTo_ArgumentNull()
+        {
+            var customQueue = new CustomQueue();
+            Assert.Throws<ArgumentNullException>(() => customQueue.CopyTo(null));
+        }
+        [Fact]
+        public void Dequeue_RemovesTheItemAtTheHeadOfTheQueueAndReturnsIt()
+        {
+            var customQueue = new CustomQueue();
+            customQueue.Enqueue(1);
+            customQueue.Dequeue();
+            Assert.False(customQueue.Contains(1));
+            Assert.Equal(0, customQueue.Count);
+        }
+        [Fact]
+        public void Dequeue_EmptyQueue()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var customQueue = new CustomQueue();
+                customQueue.Dequeue();
+            });
+        }
+        [Fact]
+        public void Enqueue_AddItemToTheTailOfTheQueue()
+        {
+            var customQueue = new CustomQueue(2);
+            customQueue.Enqueue(1);
+            Assert.True(customQueue.Contains(1));
+            Assert.True(customQueue.Count == 1);
+        }
+        [Fact]
+        public void Enqueue_CountNumbersEqualsMaxCount()
+        {
+            var customQueue = new CustomQueue(2);
+            customQueue.Enqueue(1);
+            customQueue.Enqueue(2);
+            customQueue.Enqueue(3);
+            Assert.False(customQueue.Contains(1));
+            Assert.True(customQueue.Contains(3));
+        }
+        [Fact]
+        public void Equals_EqualOneClassWithOther()
+        {
+            var customQueue = new CustomQueue(2);
+            customQueue.Enqueue(1);
+            var newCustomQueue = new CustomQueue(2);
+            newCustomQueue.Enqueue(1);
+            Assert.True(customQueue.Equals(newCustomQueue));
+        }
+        [Fact]
+        public void Equals_ArgumentNull()
+        {
+            var customQueue = new CustomQueue(2);
+            Assert.False(customQueue.Equals(null));
+        }
+        [Fact]
+        public void Equals_FirstQueueMaxCountLessThanSecond()
+        {
+            var customQueue = new CustomQueue(2);
+            customQueue.Enqueue(1);
+            var newCustomQueue = new CustomQueue(3);
+            newCustomQueue.Enqueue(1);
+            Assert.False(customQueue.Equals(newCustomQueue));
+        }
+        [Fact]
+        public void MoveNext_MovesTheEnumeratorToTheNextElementOfTheQueue()
+        {
+            var customQueue = new CustomQueue(10);
+            var actual = 0;
+            customQueue.MoveNext(ref actual);
+            Assert.Equal(1, actual);
+        }
+        [Fact]
+        public void MoveNext_ArgumentOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var customQueue = new CustomQueue();
+                var actual = -1;
+                customQueue.MoveNext(ref actual);
+            });
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var customQueue = new CustomQueue(10);
+                var actual = 11;
+                customQueue.MoveNext(ref actual);
+            });
+        }
+        [Fact]
+        public void Peek_ReturnsTheItemAtTheHeadOfTheQueue()
+        {
+            var customQueue = new CustomQueue();
+            customQueue.Enqueue(1);
+            customQueue.Enqueue(2);
+            customQueue.Enqueue(3);
+            Assert.Equal(1, customQueue.Peek());
+        }
+        [Fact]
+        public void Peek_EmptyQueue()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var customQueue = new CustomQueue();
+                customQueue.Peek();
+            });
+        }
+    }
+}
---
+++ Task1.Tests/CustomStackTests.cs
@@ -0,0 +1,126 @@
+﻿namespace Task1.CustomStack
+{
+    public class CustomStackTests
+    {
+        [Fact]
+        public void Clear_RemovesAllItemsOfTheQueue()
+        {
+            var customStack = new CustomStack();
+            customStack.Push(1);
+            customStack.Push(2);
+            customStack.Clear();
+            Assert.Equal(0, customStack.Count);
+        }
+        [Fact]
+        public void Contains_StackContainsAtLeastOneObjectEqualToItem()
+        {
+            var customStack = new CustomStack();
+            customStack.Push(1);
+            Assert.True(customStack.Contains(1));
+            Assert.False(customStack.Contains(2));
+        }
+        [Fact]
+        public void Contains_StackIsEmpty()
+        {
+            var customStack = new CustomStack();
+            Assert.False(customStack.Contains(1));
+        }
+        [Fact]
+        public void CopyTo_CopiesElementsFromOneCollectionToAnother()
+        {
+            var customStack = new CustomStack();
+            customStack.Push(1);
+            var newCustomQueue = new CustomStack();
+            customStack.CopyTo(newCustomQueue);
+            Assert.True(customStack.Equals(newCustomQueue));
+        }
+        [Fact]
+        public void CopyTo_ArgumentNull()
+        {
+            var customStack = new CustomStack();
+            Assert.Throws<ArgumentNullException>(() => customStack.CopyTo(null));
+        }
+        [Fact]
+        public void Equals_EqualOneClassWithOther()
+        {
+            var customStack = new CustomStack();
+            customStack.Push(1);
+            var newCustomStack = new CustomStack();
+            newCustomStack.Push(1);
+            Assert.True(customStack.Equals(newCustomStack));
+        }
+        [Fact]
+        public void Equals_ArgumentNull()
+        {
+            var customStack = new CustomStack();
+            Assert.False(customStack.Equals(null));
+        }
+        [Fact]
+        public void Equals_FirstStackMaxCountLessThanSecond()
+        {
+            var customQueue = new CustomStack(2);
+            customQueue.Push(1);
+            var newCustomQueue = new CustomStack(3);
+            newCustomQueue.Push(1);
+            Assert.False(customQueue.Equals(newCustomQueue));
+        }
+        [Fact]
+        public void Peek_ReturnsTheItemAtTheBeginningOfTheStackWithoutDeletingIt()
+        {
+            var customStack = new CustomStack();
+            customStack.Push(1);
+            customStack.Push(2);
+            customStack.Push(3);
+            Assert.Equal(3, customStack.Peek());
+        }
+        [Fact]
+        public void Peek_StackIsEmpty()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var newCustomQueue = new CustomStack();
+                newCustomQueue.Peek();
+            });
+        }
+        [Fact]
+        public void Pop_RemovesAndReturnsTheItemAtTheBeginningOfTheStack()
+        {
+            var customStack = new CustomStack();
+            customStack.Push(1);
+            customStack.Pop();
+            Assert.False(customStack.Contains(1));
+        }
+        [Fact]
+        public void Pop_StackIsEmpty()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var newCustomStack = new CustomStack();
+                newCustomStack.Push(1);
+                newCustomStack.Push(2);
+                newCustomStack.Pop();
+                newCustomStack.Pop();
+                newCustomStack.Pop();
+            });
+        }
+        [Fact]
+        public void Push_InsertsAnItemAsTheTopElementOfTheStack()
+        {
+            var customStack = new CustomStack(1);
+            customStack.Push(1);
+            Assert.True(customStack.Contains(1));
+            Assert.True(customStack.Count == 1);
+        }
+        [Fact]
+        public void Push_StackOverflow()
+        {
+            Assert.Throws<StackOverflowException>(() =>
+            {
+                var newCustomStack = new CustomStack(2);
+                newCustomStack.Push(1);
+                newCustomStack.Push(2);
+                newCustomStack.Push(3);
+            });
+        }
+    }
+}

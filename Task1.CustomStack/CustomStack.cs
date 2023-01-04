9800166db00098464088410aac64f0e78965d5b2
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-01-04 12:45:41 +0200

    Write a summary for methods

--- Task1.CustomStack/CustomStack.cs
+++ Task1.CustomStack/CustomStack.cs
@@ -10,22 +10,42 @@ namespace Task1.CustomStack
     public class CustomStack
     {
         private int[] array;
-        private readonly int size = 10;// Set stack size.
-        public int MaxCount => array.Length;// Max number of elements.
-        public int Count { get; private set; }// Number of elements.
-        //Create a stack with a defalt size 10
+        /// <summary>
+        /// Set stack size.
+        /// </summary>
+        private readonly int size = 10;
+        /// <summary>
+        /// Max number of elements.
+        /// </summary>
+        public int MaxCount => array.Length;
+        /// <summary>
+        /// Number of elements.
+        /// </summary>
+        public int Count { get; private set; }
+        /// <summary>
+        /// Create a stack with a defalt size 10
+        /// </summary>
+        /// <param name="size"></param>
         public CustomStack(int size = 10)
         {
             array = new int[size];
             this.size = size;
         }
-        //Create a stack with a defalt size 10 and specified data
+        /// <summary>
+        /// Create a stack with a defalt size 10 and specified data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="size"></param>
         public CustomStack(int data, int size = 10) : this(size)
         {
             array[Count] = data;
             Count++;
         }
-        // Inserts an item as the top element of the stack
+        /// <summary>
+        /// Inserts an item as the top element of the stack
+        /// </summary>
+        /// <param name="item"></param>
+        /// <exception cref="StackOverflowException"></exception>
         public void Push(int item)
         {
             if (Count < size)
@@ -38,7 +58,11 @@ namespace Task1.CustomStack
                 throw new StackOverflowException("Stack overflow");
             }
         }
-        //Removes and returns the object at the beginning of the stack .
+        /// <summary>
+        /// Removes and returns the object at the beginning of the stack .
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public int Pop()
         {
             if (Count > 0)
@@ -52,8 +76,11 @@ namespace Task1.CustomStack
                 throw new InvalidOperationException("Stack is empty");
             }
         }
-        //Returns the object at the beginning of the stack without deleting it.
-        //If the stack is empty, this method throws a NullReferenceException
+        /// <summary>
+        ///Returns the object at the beginning of the stack without deleting it.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public int Peek()
         {
             if (Count > 0)
@@ -65,8 +92,12 @@ namespace Task1.CustomStack
                 throw new InvalidOperationException("Stack is empty");
             }
         }
-        // Returns true if the stack contains at least one object equal to item.
-        // If the stack is empty, this method throws a false
+        /// <summary>
+        /// Returns true if the stack contains at least one object equal to item.
+        /// If the stack is empty, this method throws a false
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Contains(int item)
         {
             if (Count == 0)
@@ -80,8 +111,12 @@ namespace Task1.CustomStack
             }
             return false;
         }
-        // CopyTo copies a collection into an Array, starting at a particular
-        // index into the array.
+        /// <summary>
+        /// CopyTo copies a collection into an Array, starting at a particular
+        /// index into the array.
+        /// </summary>
+        /// <param name="stack"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void CopyTo(CustomStack stack)
         {
             if (stack == null)
@@ -96,17 +131,23 @@ namespace Task1.CustomStack
                 stack.Push(item);
 
         }
-        // Removes all Objects from the stack.
+        /// <summary>
+        /// Removes all Objects from the stack.
+        /// </summary>
         public void Clear()
         {
             if (Count != 0)
             {
-                Array.Clear(array,0,MaxCount);
+                Array.Clear(array, 0, MaxCount);
             }
             Count = 0;
         }
-        //Used to check whether two specified objects have the same value or not.
-        //If both objects have same value, it return true otherwise false. 
+        /// <summary>
+        /// Used to check whether two specified objects have the same value or not.
+        /// If both objects have same value, it return true otherwise false. 
+        /// </summary>
+        /// <param name="stack"></param>
+        /// <returns></returns>
         public bool Equals(CustomStack stack)
         {
             if (stack == null)

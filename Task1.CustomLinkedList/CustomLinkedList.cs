9a4a2155f920ed5d398f3eb126a8da2f89e02179
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-01-16 14:11:24 +0200

    Create a CustomLinkedList with test and diagram

--- Task1.CustomLinkedList/CustomLinkedList.cs
+++ Task1.CustomLinkedList/CustomLinkedList.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Task1.CustomLinkedList
 {
@@ -16,11 +19,11 @@ namespace Task1.CustomLinkedList
         /// <summary>
         /// First element in the list.
         /// </summary>
-        public Item Head { get; private set; }
+        public Node Head { get; private set; }
         /// <summary>
         /// Last element in the list.
         /// </summary>
-        public Item Tail { get; private set; }
+        public Node Tail { get; private set; }
         /// <summary>
         /// Count elements in the list.
         /// </summary>
@@ -40,74 +43,88 @@ namespace Task1.CustomLinkedList
         {
             SetHaidAndTail(data);
         }
+        /// <summary>
+        /// Traverse from head and print all nodes value.
+        /// </summary>
+        public void PrintAllNodes()
+        {
+            var curr = Head;
+            if (Head == null)
+            {
+               throw new NullReferenceException("List is empty ");
+            }
+            Console.Write("Nodes of doubly linked list: Head ->");
+            while (curr != null)
+            {
+                Console.Write(curr.Data);
+                curr = curr.Next;
+                Console.Write("->");
+            }
+            Console.Write("NULL \n");
+        }
+        /// <summary>
+        /// Set data to the head and tail in the list.
+        /// </summary>
+        /// <param name="data"></param>
         private void SetHaidAndTail(int data)
         {
-            var item = new Item(data);
+            var item = new Node(data);
             Head = item;
             Tail = item;
             Count = 1;
         }
         /// <summary>
-        /// Add data to the tail of the list.
-        /// </summary>
-        /// <param name="data"></param>
-        public void Add(int data)
-        {
-            if (Tail != null)
-            {
-                var item = new Item(data);
-                Tail.Next = item;
-                Tail = item;
-                Count++;
-            }
-            else
-            {
-                SetHaidAndTail(data);
-            }
-        }
-        /// <summary>
         /// Delete first occurrence of data in the list.
         /// </summary>
         /// <param name="data"></param>
-        public void Delete(int data)
+        public void Remove(int data)
         {
-            if (Head != null)
+            var current = Head;
+            while (current != null)
             {
-                if (Head.Data.Equals(data))
+                if (current.Data == data)
                 {
-                    Head = Head.Next;
-                    Count--;
-                    return;
-                }
-                var current = Head.Next;
-                var previous = Head;
-                while (current != null)
-                {
-                    if (current.Data.Equals(data))
+                    if (current.Next == null)
                     {
-                        previous.Next = current.Next;
-                        Count--;
-                        return;
+                        Tail = current.Previous;
                     }
-                    previous = current;
-                    current = current.Next;
+                    else
+                    {
+                        current.Next.Previous = current.Previous;
+                    }
+                    if (current.Previous == null)
+                    {
+                        Head = current.Next;
+                    }
+                    else
+                    {
+                        current.Previous.Next = current.Next;
+                    }
+                    current = null;
+                    Count--;
+                    return ;
                 }
-            }
-            else
-            {
-                SetHaidAndTail(data);
+                current = current.Next;
             }
         }
         /// <summary>
         /// Add data to the head of the list.
         /// </summary>
         /// <param name="data"></param>
-        public void AppendHead(int data)
+        public void AddFirst(int data)
         {
-            var item = new Item(data)
+            var item = new Node(data)
             {
                 Next = Head
             };
+            if (Head == null)
+            {
+                Tail = item;
+            }
+            else
+            {
+                Head.Previous = item;
+            }
             Head = item;
             Count++;
         }
@@ -115,12 +132,37 @@ namespace Task1.CustomLinkedList
         /// Add data to the tail of the list.
         /// </summary>
         /// <param name="data"></param>
-        public void AppendTail(int data)
+        public void AddLast(int data)
         {
-            var item = new Item(data)
+            var item = new Node(data);
+            if (Tail == null)
             {
-                Next = Tail
-            };
+                Head = item;
+            }
+            else
+            {
+                item.Previous = Tail;
+                Tail.Next = item;
+            }
+            Tail = item;
+            Count++;
+        }
+        /// <summary>
+        /// Add data to the tail of the list.
+        /// </summary>
+        /// <param name="data"></param>
+        private void Add(Node data)
+        {
+            var item = new Node(data);
+            if (Tail == null)
+            {
+                Head = item;
+            }
+            else
+            {
+                item.Previous = Tail;
+                Tail.Next = item;
+            }
             Tail = item;
             Count++;
         }
@@ -138,7 +180,7 @@ namespace Task1.CustomLinkedList
                 {
                     if (current.Data.Equals(target))
                     {
-                        var item = new Item(data);
+                        var item = new Node(data);
                         item.Next = current.Next;
                         current.Next = item;
                         Count++;
@@ -152,9 +194,8 @@ namespace Task1.CustomLinkedList
             }
             else
             {
-                //If list is empty add data or stay empty
-                //SetHaidAndTail(target);
-                // Add(data);
+                SetHaidAndTail(target);
+                AddLast(data);
             }
         }
         /// <summary>
@@ -164,31 +205,78 @@ namespace Task1.CustomLinkedList
         /// <param name="data"></param>
         public void InsertBefore(int target, int data)
         {
-            if (Head != null)
+            if (Head.Data == target)
             {
-                var current = Head;
-                while (current != null)
-                {
-                    if (current.Data.Equals(target))
-                    {
-                        var item = new Item(data);
-                        item.Next = current.Next;
-                        current.Next = item;
-                        Count++;
-                        return;
-                    }
-                    else
-                    {
-                        current = current.Next;
-                    }
-                }
+                var current = new Node(data);
+                current.Next = Head;
+                Head = current;
+                return;
             }
             else
             {
-                //If list is empty add data or stay empty
-                //SetHaidAndTail(target);
-                // Add(data);
+                Node secondCurrent = null;
+                for (var current = Head; current.Data != target;
+                    secondCurrent = current, current = current.Next) ;
+                var thirdCurrent = new Node(data);
+                thirdCurrent.Next = secondCurrent.Next;
+                secondCurrent.Next = thirdCurrent;
+                return;
             }
+        }
+        /// <summary>
+        /// Remove first element in the list.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        public void RemoveFirst()
+        {
+            if (Head != null)
+            {
+                Head = Head.Next;
+                if (Head == null)
+                {
+                    Tail = null;
+                }
+                Count--;
+            }
+            else
+            {
+                throw new InvalidOperationException("The linked list is empty");
+            }
+        }
+        /// <summary>
+        /// Remove last element in the list.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        public void RemoveLast()
+        {
+            if (Head == null && Tail == null) throw new InvalidOperationException("The list is empty");
+
+            Tail = Tail.Previous;
+            Tail.Next = null;
+            Count--;
+        }
+        /// <summary>
+        /// Returns true if the list contains at least one object equal to item.
+        /// If the list is empty, this method throws a false.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(int data)
+        {
+            if (Count == 0)
+            {
+                return false;
+            }
+            var temp = Head;
+            for (int i = 0; i < Count; i++)
+            {
+                if (temp.Data.Equals(data))
+                {
+                    return true;
+                }
+                temp = temp.Next;
+            }
+            return false;
         }
         /// <summary>
         /// Clear list.
@@ -199,6 +287,81 @@ namespace Task1.CustomLinkedList
             Tail = null;
             Count = 0;
         }
+        /// <summary>
+        /// CopyTo copies elements from one collection to another
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void CopyTo(CustomLinkedList list)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException("Argument null exception");
+            }
+            if (Count == 0) return;
+            var curr = Head;
+            while (curr != null)
+            {
+                list.Add(curr.Copy());
+                curr = curr.Next;
+            }
 
+        }
+        /// <summary>
+        /// Used to check whether two specified objects have the same value or not.
+        ///If both objects have same value, it return true otherwise false.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        public bool Equals(CustomLinkedList secondlist)
+        {
+            Node a = Head, b = secondlist.Head;
+            while (a != null && b != null)
+            {
+                if (a.Data != b.Data)
+                    return false;
+                a = a.Next;
+                b = b.Next;
+            }
+            return (a == null && b == null);
+        }
+        /// <summary>
+        /// Finds the first item that contains the specified data.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public Node FindFirst(int data)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Data == data)
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+        /// <summary>
+        /// Finds the last item that contains the specified data.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public Node FindLast(int data)
+        {
+            var current = Tail;
+            while (current != null)
+            {
+                if (current.Data == data)
+                {
+                    return current;
+                }
+                current = current.Previous;
+            }
+            return null;
+        }
     }
+
 }
+
---
+++ Task1.CustomLinkedList/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task1.CustomLinkedList
+{
+    /// <summary>
+    /// List cell.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// The data stored in the list cell.
+        /// </summary>
+        public int Data { get; set; }
+        /// <summary>
+        /// Next list cell.
+        /// </summary>
+        public Node Next { get; set; }
+        /// <summary>
+        /// Previous list cell.
+        /// </summary>
+        public Node Previous { get; set; }
+        /// <summary>
+        /// Item without data
+        /// </summary>
+        public Node() { }
+        public Node(Node data)
+        { 
+            Data = data.Data;
+        }
+        /// <summary>
+        /// Item with data.
+        /// </summary>
+        /// <param name="data"></param>
+        public Node(int data)
+        {
+            Data = data;
+        }
+        /// <summary>
+        /// Copy elments from one collection to other.
+        /// </summary>
+        /// <returns></returns>
+        public Node Copy()
+        {
+            return new Node(Data);
+        }
+    }
+}

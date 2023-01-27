7bf3760ea498357bc1af378360093556c5bd53dc
Oleksii Lesko <Oleksii.Lesko@nice.com>
2023-01-27 12:12:18 +0200

    Сhanged the built-in exceptionsin  ones

--- Task1.CustomExceptions/LessThenNecessaryException.cs
+++ Task1.CustomExceptions/LessThenNecessaryException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task1.CustomExceptions
+﻿namespace Task1.CustomExceptions
 {
     public class LessThenNecessaryException : Exception
     {
--- Task1.CustomLinkedList/CustomLinkedList.cs
+++ Task1.CustomLinkedList/CustomLinkedList.cs
@@ -27,11 +27,7 @@ namespace Task1.CustomLinkedList
         /// </summary>
         /// <param name="position"></param>
         /// <returns></returns>
-<<<<<<< HEAD
-        /// <exception cref="EmptyException"></exception>
-=======
         /// <exception cref="LessThenNecessaryException"></exception>
->>>>>>> feature-exception
         /// <exception cref="OverFlowException"></exception>
         public T this[int position]
         {
@@ -39,19 +35,11 @@ namespace Task1.CustomLinkedList
             {
                 if (position < 0)
                 {
-<<<<<<< HEAD
-                    throw new EmptyException("Position less then count of elements in the CustomStack");
-                }
-                if (position > Count)
-                {
-                    throw new OverFlowException("Position more then count of elements in the CustomStack");
-=======
-                    throw new LessThenNecessaryException("Position less  then count of elements in the CustomLinkedList");
+                    throw new LessThenNecessaryException("Position less then count of elements in the CustomStack");
                 }
                 if(position > Count)
                 {
                     throw new OverFlowException("Position more  then count of elements in the CustomLinkedList");
->>>>>>> feature-exception
                 }
                 Node<T> node = Head;
                 for (int i = 0; i < position; i++)
@@ -64,19 +52,11 @@ namespace Task1.CustomLinkedList
             {
                 if (position < 0)
                 {
-<<<<<<< HEAD
-                    throw new EmptyException("Position less then count of elements in the CustomStack");
+                    throw new LessThenNecessaryException("Position less then count of elements in the CustomStack");
                 }
                 if (position > Count)
                 {
                     throw new OverFlowException("Position more then count of elements in the CustomStack");
-=======
-                    throw new LessThenNecessaryException("Position less  then count of elements in the CustomLinkedList");
-                }
-                if (position > Count)
-                {
-                    throw new OverFlowException("Position more  then count of elements in the CustomLinkedList");
->>>>>>> feature-exception
                 }
                 Node<T> node = Head;
                 for (int i = 0; i < position; i++)
